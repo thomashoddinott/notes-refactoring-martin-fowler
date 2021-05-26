b1702ceb41d764c604470df3222ca755f2cd46e5
Thomas Hoddinott <thomashoddinott477@gmail.com>
2021-05-26 09:33:08 +0100

    ...

--- foo.js
+++ foo.js
@@ -1,6 +1,14 @@
 function doSomeMath(a, b) {
-    const c = a / b
+    const c = a + b
     const d = 1 // unused
-    console.log(`quotient ${a} / ${b} = ${c}`)
+    const d = 1 // unused
+    const d = 1 // unused
+    const d = 1 // unused
+    const d = 1 // unused
+    const d = 1 // unused
+    const d = 1 // unused
+    const d = 1 // unused
+    const d = 1 // unused
+    console.log(`sum ${a} + ${b} = ${c}`)
 }
 doSomeMath(2, 3)
