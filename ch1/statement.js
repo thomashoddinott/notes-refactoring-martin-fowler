9bfbb0efcd00878be78072ee3f37b51a72eb3e84
Thomas Hoddinott <thomashoddinott477@gmail.com>
2021-06-02 09:19:34 +0100

    move function (page 29)

--- ch1/statement.js
+++ ch1/statement.js
@@ -8,6 +8,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result)
     result.amount = amountFor(result)
+    result.volumeCredits = volumeCreditsFor(result)
     return result
   }
 
@@ -37,6 +38,13 @@ function statement(invoice, plays) {
     return result;
   }
 
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
 }
 
 function renderPlainText(data) {
@@ -59,7 +67,7 @@ function renderPlainText(data) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf)
+      result += perf.volumeCredits
     }
     return result;
   }
@@ -72,12 +80,7 @@ function renderPlainText(data) {
       }).format(aNumber / 100);
   }
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
+
 }
 
 
