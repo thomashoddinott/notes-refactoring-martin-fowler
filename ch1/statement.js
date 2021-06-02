2926f5b16307197c73c3acebd1417af78f2c42df
Thomas Hoddinott <thomashoddinott477@gmail.com>
2021-06-02 10:00:29 +0100

    move stuff around; make new test for html version of statement

--- ch1/statement.js
+++ ch1/statement.js
@@ -1,27 +1,29 @@
 const createStatementData = require('./createStatementData')
 
 function statement(invoice, plays) {
-  return renderPlainText(createStatementData(invoice, plays))
+  return renderHtml(createStatementData(invoice, plays))
 }
 
-function renderPlainText(data) {
-  let result = `Statement for ${data.customer}\n`;
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
   for (let perf of data.performances) {
-    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
-  result += `Amount owed is ${usd(data.totalAmount)}\n`;
-  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-      {
-        style: "currency", currency: "USD",
-        minimumFractionDigits: 2
-      }).format(aNumber / 100);
-  }
-
-
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US",
+    {
+      style: "currency", currency: "USD",
+      minimumFractionDigits: 2
+    }).format(aNumber / 100);
 }
 
 module.exports = statement
--- ch1/tests/statement.test.js
+++ ch1/tests/statement.test.js
@@ -1,10 +1,11 @@
+const statementOriginal = require('../statementOriginal')
 const statement = require('../statement')
 
 const plays = require('../data/plays.json')
 const invoices = require('../data/invoices.json')
+const invoice = invoices[0]
 
-test('1.', () => {
-  const invoice = invoices[0]
+test('statement - original', () => {
   const expected =
 `Statement for BigCo
   Hamlet: $650.00 (55 seats)
@@ -12,6 +13,21 @@ test('1.', () => {
   Othello: $500.00 (40 seats)
 Amount owed is $1,730.00
 You earned 47 credits
+`
+
+  expect(statementOriginal(invoice, plays)).toBe(expected);
+});
+
+test('statement - refactored', () => {
+  const expected =
+`<h1>Statement for BigCo</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
 `
 
   expect(statement(invoice, plays)).toBe(expected);
