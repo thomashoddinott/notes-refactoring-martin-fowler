787972bf3ca541ca42991efcb17fcf7eee6116a1
Thomas Hoddinott <thomashoddinott477@gmail.com>
2021-06-02 09:41:31 +0100

    extract function (page 30)

--- ch1/statement.js
+++ ch1/statement.js
@@ -1,10 +1,14 @@
 function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays))
+}
+
+function createStatementData(invoice, plays) {
   const statementData = {}
   statementData.customer = invoice.customer
   statementData.performances = invoice.performances.map(enrichPerformance)
   statementData.totalAmount = totalAmount(statementData)
   statementData.totalVolumeCredits = totalVolumeCredits(statementData)
-  return renderPlainText(statementData, plays)
+  return statementData
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance)
@@ -57,6 +61,7 @@ function statement(invoice, plays) {
       .reduce((total, p) => total + p.volumeCredits, 0)
   }
 }
+
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
