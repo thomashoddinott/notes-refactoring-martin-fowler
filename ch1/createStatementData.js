f43d41b2986ea47f204367a65e60637132691260
Thomas Hoddinott <thomashoddinott477@gmail.com>
2021-06-03 08:57:07 +0100

    use polymorphism + inline fn (p38)

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -4,7 +4,7 @@ class PerformanceCalculator {
         this.play = aPlay
     }
 
-    get amount () {
+    get amount() {
         let result = 0;
         switch (this.play.type) {
             case "tragedy":
@@ -26,6 +26,13 @@ class PerformanceCalculator {
         return result;
     }
 
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
+        return result;
+    }
+
 }
 
 function createStatementData(invoice, plays) {
@@ -41,19 +48,12 @@ function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance)
         result.play = calculator.play
         result.amount = calculator.amount
-        result.volumeCredits = volumeCreditsFor(result)
+        result.volumeCredits = calculator.volumeCredits
         return result
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
     }
 
     function totalAmount(data) {
