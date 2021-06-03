de79d826088bd0a098f6ec89e02d604beaf33fa5
Thomas Hoddinott <thomashoddinott477@gmail.com>
2021-06-03 09:05:44 +0100

    replace conditional with polymorphism (p39,40)

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -8,11 +8,7 @@ class PerformanceCalculator {
         let result = 0;
         switch (this.play.type) {
             case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
+                throw 'an error...'
             case "comedy":
                 result = 30000;
                 if (this.performance.audience > 20) {
@@ -36,15 +32,21 @@ class PerformanceCalculator {
 }
 
 class TragedyCalculator extends PerformanceCalculator {
-
+    get amount() {
+        let result = 40000
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30)
+        }
+        return result
+    }
 }
 
 class ComedyCalculator extends PerformanceCalculator {
-    
+
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-    switch(aPlay.type) {
+    switch (aPlay.type) {
         case "tragedy": return new TragedyCalculator(aPerformance, aPlay)
         case "comedy": return new ComedyCalculator(aPerformance, aPlay)
         default:
