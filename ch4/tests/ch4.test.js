df14a638710074828841d04809faffd5cae190e6
Thomas Hoddinott <thomashoddinott477@gmail.com>
2021-06-16 09:09:41 +0100

    2nd test

--- ch4/tests/ch4.test.js
+++ ch4/tests/ch4.test.js
@@ -1,6 +1,12 @@
 const { Province, sampleProvinceData } = require('../ch4')
 
-test('province', () => {
-    const asia = new Province(sampleProvinceData());
-    expect(asia.shortfall).toBe(5);
-});
+describe('province', () => {
+    test('shortfall', () => {
+        const asia = new Province(sampleProvinceData());
+        expect(asia.shortfall).toBe(5);
+    });
+    test('profit', () => {
+        const asia = new Province(sampleProvinceData());
+        expect(asia.profit).toBe(230);
+    });
+  });
