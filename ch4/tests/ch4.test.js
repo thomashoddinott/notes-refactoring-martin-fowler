da4bce6341f0b734e45dd46929d593978848a38c
Thomas Hoddinott <thomashoddinott477@gmail.com>
2021-06-16 09:27:43 +0100

    ... more test cases

--- ch4/tests/ch4.test.js
+++ ch4/tests/ch4.test.js
@@ -16,6 +16,16 @@ describe('province', () => {
         expect(asia.shortfall).toBe(-6);
         expect(asia.profit).toBe(292);
     })
+    test('zero demand', () => {
+        asia.demand = 0;
+        expect(asia.shortfall).toBe(-25);
+        expect(asia.profit).toBe(0);
+    })
+    test('negative demand', () => {
+        asia.demand = -1;
+        expect(asia.shortfall).toBe(-26);
+        expect(asia.profit).toBe(-10);
+    })
 });
 
 describe('no producers', () => {
